46dab40c468c1cfd1036a4b1a2d2ca52c909ea93
Dave Humphreys <newky2k@users.noreply.github.com>
2018-09-28 09:20:18 +0100

    Updated to correctly implement basic mode
    Updated to correctly implement basic mode

--- DSoft.CraftyClicks.PostCodeApi/PostCodeLookupProvider.cs
+++ DSoft.CraftyClicks.PostCodeApi/PostCodeLookupProvider.cs
@@ -35,67 +35,142 @@ namespace DSoft.CraftyClicks.PostCodeApi
 
             if (jsonResponseObject != null)
             {
-                if (jsonResponseObject.delivery_points != null)
+                switch (mode)
                 {
-                    //If the node list contains address nodes then move on.
-                    int i = 0;
-
-                    foreach (var node in jsonResponseObject.delivery_points)
-                    {
-                        ClsAddress address = new ClsAddress()
+                    case AddressMode.Rapid:
                         {
-                            AddressID = i,
-                            AddressLine1 = node.line_1,
-                            AddressLine2 = node.line_2,
-                            Department = node.department_name,
-                            Organisation = node.organisation_name,
+                            if (jsonResponseObject.delivery_points != null)
+                            {
+                                //If the node list contains address nodes then move on.
+                                int i = 0;
 
-                            County = jsonResponseObject.traditional_county,
-                            PostCode = jsonResponseObject.postcode,
-                            Town = jsonResponseObject.town
-                        };
+                                foreach (var node in jsonResponseObject.delivery_points)
+                                {
+                                    ClsAddress address = new ClsAddress()
+                                    {
+                                        AddressID = i,
+                                        AddressLine1 = node.line_1,
+                                        AddressLine2 = node.line_2,
+                                        Department = node.department_name,
+                                        Organisation = node.organisation_name,
 
-                        result.Addresses.Add(address);
-                        i++;
-                    }
-                }
-                else
-                {
-                    foreach (var node in jsonResponseObject)
-                    {
-                        // Get the details of the error message and return it the user.
-                        switch ((string)node.Value)
-                        {
-                            case "0001":
-                                result.Status = QueryStatus.PostCodeNotFound;
-                                break;
-                            case "0002":
-                                result.Status = QueryStatus.InvalidPostCodeFormat;
-                                break;
-                            case "7001":
-                                result.Status = QueryStatus.DemoLimitExceeded;
-                                break;
-                            case "8001":
-                                result.Status = QueryStatus.InvalidOrNoAccessToken;
-                                break;
-                            case "8003":
-                                result.Status = QueryStatus.AccountCrediteAllowanceExceeded;
-                                break;
-                            case "8004":
-                                result.Status = QueryStatus.AccessDeniedDueToAccessRules;
-                                break;
-                            case "8005":
-                                result.Status = QueryStatus.AccessDeniedAccountSuspended;
-                                break;
-                            case "9001":
-                                result.Status = QueryStatus.InternalServerError;
-                                break;
-                            default:
-                                result.Status = QueryStatus.Unknown;
-                                break;
+                                        County = jsonResponseObject.traditional_county,
+                                        PostCode = jsonResponseObject.postcode,
+                                        Town = jsonResponseObject.town
+                                    };
+
+                                    result.Addresses.Add(address);
+                                    i++;
+                                }
+                            }
+                            else
+                            {
+                                foreach (var node in jsonResponseObject)
+                                {
+                                    // Get the details of the error message and return it the user.
+                                    switch ((string)node.Value)
+                                    {
+                                        case "0001":
+                                            result.Status = QueryStatus.PostCodeNotFound;
+                                            break;
+                                        case "0002":
+                                            result.Status = QueryStatus.InvalidPostCodeFormat;
+                                            break;
+                                        case "7001":
+                                            result.Status = QueryStatus.DemoLimitExceeded;
+                                            break;
+                                        case "8001":
+                                            result.Status = QueryStatus.InvalidOrNoAccessToken;
+                                            break;
+                                        case "8003":
+                                            result.Status = QueryStatus.AccountCrediteAllowanceExceeded;
+                                            break;
+                                        case "8004":
+                                            result.Status = QueryStatus.AccessDeniedDueToAccessRules;
+                                            break;
+                                        case "8005":
+                                            result.Status = QueryStatus.AccessDeniedAccountSuspended;
+                                            break;
+                                        case "9001":
+                                            result.Status = QueryStatus.InternalServerError;
+                                            break;
+                                        default:
+                                            result.Status = QueryStatus.Unknown;
+                                            break;
+                                    }
+                                }
+                            }
+
+                            
                         }
-                    }
+                        break;
+                    case AddressMode.Basic:
+                        {
+                            if (jsonResponseObject.thoroughfare_count > 0 )
+                            {
+                                //If the node list contains address nodes then move on.
+                                int i = 0;
+
+                                foreach (var node in jsonResponseObject.thoroughfares)
+                                {
+                                    ClsAddress address = new ClsAddress()
+                                    {
+                                        AddressID = i,
+                                        AddressLine1 = node.line_1,
+                                        AddressLine2 = node.line_2,
+                                        Department = node.department_name,
+                                        Organisation = node.organisation_name,
+
+                                        County = jsonResponseObject.traditional_county,
+                                        PostCode = jsonResponseObject.postcode,
+                                        Town = jsonResponseObject.town
+                                    };
+
+                                    result.Addresses.Add(address);
+                                    i++;
+                                }
+                            }
+                            else
+                            {
+                                foreach (var node in jsonResponseObject)
+                                {
+                                    // Get the details of the error message and return it the user.
+                                    switch ((string)node.Value)
+                                    {
+                                        case "0001":
+                                            result.Status = QueryStatus.PostCodeNotFound;
+                                            break;
+                                        case "0002":
+                                            result.Status = QueryStatus.InvalidPostCodeFormat;
+                                            break;
+                                        case "7001":
+                                            result.Status = QueryStatus.DemoLimitExceeded;
+                                            break;
+                                        case "8001":
+                                            result.Status = QueryStatus.InvalidOrNoAccessToken;
+                                            break;
+                                        case "8003":
+                                            result.Status = QueryStatus.AccountCrediteAllowanceExceeded;
+                                            break;
+                                        case "8004":
+                                            result.Status = QueryStatus.AccessDeniedDueToAccessRules;
+                                            break;
+                                        case "8005":
+                                            result.Status = QueryStatus.AccessDeniedAccountSuspended;
+                                            break;
+                                        case "9001":
+                                            result.Status = QueryStatus.InternalServerError;
+                                            break;
+                                        default:
+                                            result.Status = QueryStatus.Unknown;
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                        break;
                 }
+
             }
             else
             {
